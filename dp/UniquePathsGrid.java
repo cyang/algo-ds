9c734f91af5eb19bd0066956c163ef9d6bd58a08
Christopher Yang <cyang001@citymail.cuny.edu>
2016-09-04 13:48:37 -0400

    Add

--- dp/UniquePathsGrid.java
+++ dp/UniquePathsGrid.java
@@ -1,21 +1,29 @@
 public class UniquePathsGrid {
 	// Given an mxn grid find out how many unique paths can be made from the top
-	// left corner cell to the bottom right cell if you can only move down or right
-
+	// left corner cell to the bottom right cell if you can only move down or
+	// right
 	public static void main(String[] args) {
 		int m = 3;
 		int n = 7;
-		System.out.println(uniquePaths(m, n));
+		int[][] map = new int[m][n];
+
+		System.out.println(uniquePaths(m, n, map));
 	}
 
-	public static int uniquePaths(int m, int n) {
+	public static int uniquePaths(int m, int n, int[][] map) {
 		if (m == 1 && n == 1) {
 			return 0;
 		}
-		if (m > 1 && n > 1) {
-			return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
-		} else {
-			return 1;
+
+		if (map[m - 1][n - 1] != 0) {
+			return map[m - 1][n - 1];
 		}
+
+		if (m > 1 && n > 1) {
+			map[m - 1][n - 1] = uniquePaths(m - 1, n, map) + uniquePaths(m, n - 1, map);
+		} else {
+			map[m - 1][n - 1] = 1;
+		}
+		return map[m - 1][n - 1];
 	}
 }
